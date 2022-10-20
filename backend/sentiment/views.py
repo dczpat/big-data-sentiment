11d8c198ffd6f7e70ad85f9e74df320ccfa37ccf
Chenzhi <dddd@u.nus.edu>
2022-10-20 23:50:34 +0800

    add lstm && api

--- backend/sentiment/views.py
+++ backend/sentiment/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .lstm import predict_text
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
-def index(request):
-    pass
+
+@api_view(["GET", "POST"])
+def dummy(request):
+    # request should be an HTTP request
+    # which may contain several fields
+    # right now is just sentence(str) and method(str)
+    if request.method == "POST":
+        sentence = request.sentence  # TODO: serializer
+        method = request.method
+        if method == "lstm":
+            res = predict_text(sentence)
+        elif method == "bert":
+            # res=func(sentence)
+            pass
+        return Response(res)
+    else:
+        pass

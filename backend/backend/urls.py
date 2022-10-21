b81488b5c88942e82c546daaeb3edf258c120805
Chenzhi <dddd@u.nus.edu>
2022-10-21 14:18:03 +0800

    fix post req

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+
+from django.urls import include, path
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("", include("sentiment.urls")),
+    path("sentiment/", include("sentiment.urls")),
     path("admin/", admin.site.urls),
 ]
---
+++ backend/sentiment/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+# from .models import ...
+
+
+class MySerializer(serializers.BaseSerializer):
+    pass
+
+    # class Meta:
+    #     model = MarketValuations
+    #     fields = ('pk', 'marketValue', 'marketValueDate', 'createdAt', 'modifiedAt')
